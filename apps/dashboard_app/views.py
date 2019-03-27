c1330b9a7022eed55e0c8009099a71ef6764d253
Peter ONeill <jaretep@gmail.com>
2019-03-27 14:04:03 -0700

    finished clickable

--- apps/dashboard_app/views.py
+++ apps/dashboard_app/views.py
@@ -3,4 +3,16 @@ from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def index(request):
-	return render(request, "dashboard.html")
+	return redirect('dashboard')
+
+def dashboard(request):
+	return render(request, "clickable.html")
+
+def login(request):
+	return render(request, "login_page.html")
+
+def addalbum(request):
+	return render(request, "album_add.html")
+
+def addpicture(request):
+	return render(request, "picture_add.html")
